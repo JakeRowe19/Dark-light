844530a072482c300353bebb34397a1af4d492a9
JakeRowe19 <117069245+JakeRowe19@users.noreply.github.com>
2026-02-14 15:01:19 +0300

    Update menu.js WHEAT NF

--- menu.js
+++ menu.js
@@ -53,6 +53,7 @@ const BEERTYPE_BADGE = {
   "beertype=other":   "beertype=other.png",
   "beertype=n/a":     "nonalc.png",
   "beertype=wheat":   "beertype=wheat.png",
+  "beertype=wheatNF":   "beertype=wheatNF.png",
   "beertype=cider":   "beertype=cider.png",
   "beertype=mead":   "beertype=mead.png"
 };
